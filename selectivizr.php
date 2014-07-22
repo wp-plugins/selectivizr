29318e755c7005a173442c89501855f413574087
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-22 10:02:20 +0000

    tag 2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/selectivizr/tags/2.0.3@952760 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ selectivizr.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Selectivizr for WordPress
+Plugin URI: http://www.ramoonus.nl/wordpress/selectivizr/
+Description: Selectivizr is a JavaScript utility that emulates CSS3 pseudo-classes and attribute selectors in Internet Explorer 6-8. 
+Version: 2.0.3
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/
+*/
+
+// init
+function rw_selectivizr() {
+	wp_deregister_script('selectivizr',plugins_url( '/js/selectivizr.js' , __FILE__ ), array(), '1.0.3b' );
+	wp_enqueue_script( 'selectivizr' );
+} 
+// load
+add_action('wp_head', 'rw_selectivizr');
+?>
